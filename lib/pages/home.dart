84228a5ddb238cb746eb35e045796493fd768922
Prince Igwenagha <Igwep297@gmail.com>
2020-12-01 17:17:59 +0100

    added font,and made some changes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,8 +8,71 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/beefstew.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(32.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Kuusine",
+                    style: TextStyle(
+                      color: Colors.brown,
+                      fontSize: 45.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'BalsamiqSans'
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(230, 600, 20, 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FlatButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)
+                        ),
+                        onPressed: () {}, 
+                        child: Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                          'Get Started',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold
+                          ),
+                          ),
+                        ),
+                        color: Colors.brown,
+                        )
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
